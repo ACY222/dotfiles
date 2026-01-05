914027e92c4d6dfd19adca7cb077811f7dc1f51a
ACY222 <838113915@qq.com>
2026-01-05 16:31:05 +0800

    use gitsigns instead of gitgutter

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,19 +1,55 @@
 return {
-  'airblade/vim-gitgutter',
-  event = { 'BufReadPre', 'BufNewFile' },
-  config = function()
-    vim.cmd([[
-      nmap ]h <Plug>(GitGutterNextHunk)
-      nmap [h <Plug>(GitGutterPrevHunk)
-      nmap ghs <Plug>(GitGutterStageHunk)
-      nmap ghu <Plug>(GitGutterUndoHunk)
-      nmap ghp <Plug>(GitGutterPreviewHunk)
-      let g:gitgutter_sign_added = '+'
-      let g:gitgutter_sign_modified = '~'
-      let g:gitgutter_sign_removed = '-'
-      let g:gitgutter_sign_removed_first_line = '¯'
-      let g:gitgutter_sign_removed_above_and_below = '='
-      let g:gitgutter_sign_modified_removed = '~_'
-    ]])
-  end,
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+        signs = {
+            add = { text = "+" },
+            change = { text = "~" },
+            delete = { text = "-" },
+            topdelete = { text = "¯" },
+            changedelete = { text = "~_" },
+            untracked = { text = "┆" },
+        },
+
+        on_attach = function(bufnr)
+            local gs = package.loaded.gitsigns
+
+            local function map(mode, l, r, desc)
+                vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+            end
+
+            -- next hunk
+            map("n", "]h", function()
+                if vim.wo.diff then
+                    return "]c"
+                end
+                vim.schedule(function()
+                    gs.next_hunk()
+                end)
+                return "<Ignore>"
+            end, "Next Hunk")
+
+            -- previous hunk
+            map("n", "[h", function()
+                if vim.wo.diff then
+                    return "[c"
+                end
+                vim.schedule(function()
+                    gs.prev_hunk()
+                end)
+                return "<Ignore>"
+            end, "Prev Hunk")
+
+            map("n", "ghs", gs.stage_hunk, "Stage Hunk")
+            map("n", "ghr", gs.reset_hunk, "Reset Hunk")
+            map("n", "ghp", gs.preview_hunk, "Preview Hunk")
+
+            -- check the git history of current line
+            map("n", "ghb", function()
+                gs.blame_line({ full = true })
+            end, "Blame Line")
+            -- git diff
+            map("n", "ghd", gs.diffthis, "Diff This")
+        end,
+    },
 }
