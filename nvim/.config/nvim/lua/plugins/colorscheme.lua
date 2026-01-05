518dbb0cff563b6f26f2f9f6ec62fcb4b51ef05d
ACY222 <838113915@qq.com>
2026-01-05 16:23:04 +0800

    organize colorscheme and comment

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,35 +1,41 @@
 return {
-  -- add dracula
-  "Mofiqul/dracula.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function ()
-    require('dracula').setup({
-      transparent_bg = true,
-      italic_comment = true,
-      lualine_bg_color = "#44475a",
-    })
-    vim.cmd([[colorscheme dracula]])
-  end,
+    "Mofiqul/dracula.nvim",
+    lazy = false,
+    priority = 1000,
+
+    opts = {
+        transparent_bg = true,
+        italic_comment = true,
+        lualine_bg_color = "#44475a",
+    },
+
+    config = function(_, opts)
+        local dracula = require("dracula")
+        dracula.setup(opts)
+
+        vim.cmd.colorscheme("dracula")
+    end,
 }
 -- return {
---   -- colorscheme
---   'folke/tokyonight.nvim',
---   lazy = false,           -- load it during startup
---   priority = 1000,        -- load this before all other plugins
---   config = function()
---     require('tokyonight').setup({
---       style = 'storm',
---       transparent = true,
---       terminal_colors = true,
---       styles = {
---         comment = { bold = true },
---         keywords = { italic = true },
---         sidebars = 'transparent',
---         floats = 'transparent',
---       },
---     })
---     -- load the colorscheme here
---     vim.cmd([[colorscheme tokyonight-storm]])
---   end,
+--     -- colorscheme
+--     "folke/tokyonight.nvim",
+--     lazy = false, -- load it during startup
+--     priority = 1000, -- load this before all other plugins
+--     opts = {
+--         style = "storm",
+--         transparent = true,
+--         terminal_colors = true,
+--         styles = {
+--             comment = { bold = true },
+--             keywords = { italic = true },
+--             sidebars = "transparent",
+--             floats = "transparent",
+--         },
+--     },
+--     config = function(_, opts)
+--         local tokyonight = require("tokyonight")
+--         tokyonight.setup(opts)
+--         -- load the colorscheme here
+--         vim.cmd.colorscheme("tokyonight")
+--     end,
 -- }
