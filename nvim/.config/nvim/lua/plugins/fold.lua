b24b2aeac5c4b5af4dec4268e02a0a54f9bb1990
ACY222 <838113915@qq.com>
2026-01-05 16:37:17 +0800

    use 4 as indent instead of 2

--- nvim/.config/nvim/lua/plugins/fold.lua
+++ nvim/.config/nvim/lua/plugins/fold.lua
@@ -1,5 +1,5 @@
 -- fold in all filetypes
 return {
-  -- use za to fold, zR to openAllFolds, zM to closeAllFolds
-  'kevinhwang91/nvim-ufo',
+    -- use za to fold, zR to openAllFolds, zM to closeAllFolds
+    "kevinhwang91/nvim-ufo",
 }
--- nvim/.config/nvim/lua/plugins/leetcode.lua
+++ nvim/.config/nvim/lua/plugins/leetcode.lua
@@ -2,15 +2,14 @@
 -- You need to copy the cookies from the leetcode
 --  just copy the value of Cookie of the website in safari
 return {
-  "kawre/leetcode.nvim",
-  dependencies = {
-    -- include a picker of your choice, see picker section for more details
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-  },
-  build = ":LeetInstall",
-  config = function()
-    require("leetcode").setup({
-    })
-  end,
+    "kawre/leetcode.nvim",
+    dependencies = {
+        -- include a picker of your choice, see picker section for more details
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+    },
+    build = ":LeetInstall",
+    config = function()
+        require("leetcode").setup({})
+    end,
 }
