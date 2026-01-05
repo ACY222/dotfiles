074511e1b9e7d29ad894911ee0770c88c05ceeed
ACY222 <838113915@qq.com>
2026-01-05 15:33:29 +0800

    new plguin `lazydev` to remove the warnings about `vim.*`

---
+++ nvim/.config/nvim/lua/plugins/lua.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        -- remove the warnings about `Undefined global "vim"`
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                { path = "luvit-meta/library", words = { "vim%.uv" } },
+            },
+        },
+    },
+    { "Bilal2453/luvit-meta", lazy = true },
+}
