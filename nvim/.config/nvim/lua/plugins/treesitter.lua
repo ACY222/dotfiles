b00e3df98f27512a82545ad5680075f9764870f3
ACY222 <838113915@qq.com>
2026-01-06 11:12:45 +0800

    reconstruct treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,86 +1,85 @@
 return {
-  {
-    -- highlight, indent, fold function by syntax analyzing
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,           -- it doesn't support lazy-load
-    priority = 1000,
-    build = ":TSUpdate",
-    config = function()
-      vim.opt.autoindent = false
-      vim.opt.cindent = false
+    {
+        -- highlight, indent, fold function by syntax analyzing
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPost", "BufNewFile" },
+        build = ":TSUpdate",
+        opts = {
+            auto_install = true,
+            ensure_installed = {
+                -- main languages
+                "c",
+                "cpp",
+                "python",
+                "markdown",
+                "typst",
+                -- config languages
+                "lua",
+                "vim",
+            },
+            highlight = {
+                enable = true,
+                -- disable highlight for large files
+                disable = function(_, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local buf_name = vim.api.nvim_buf_get_name(buf)
 
-      local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
-      if not status_ok then
-        return
-      end
+                    local stats = vim.uv.fs_stat(buf_name)
+                    if stats and stats.size > max_filesize then
+                        return true
+                    end
 
-      treesitter.setup {
-        modules = {},
-        ignore_install = {},
-
-        auto_install = true,
-        sync_install = true,
-        ensure_installed = {
-          -- my main languages
-          "c", "cpp", "python", "markdown", "typst",
-          -- my config languages
-          "lua", "vim",
+                    -- if there super long lines
+                    local line_count = vim.api.nvim_buf_line_count(buf)
+                    if line_count > 5000 then
+                        return true
+                    end
+                end,
+            },
+            indent = {
+                enable = true,
+            },
+            -- increase selection scope gradually
+            incremental_selection = {
+                enable = false,
+                keymaps = {
+                    -- use <CR> to trigger selection and increase scope
+                    -- <BS> to decrease scope
+                    init_selection = "<CR>",
+                    node_incremental = "<CR>",
+                    scope_incremental = false,
+                    node_decremental = "<BS>",
+                },
+            },
         },
-        highlight = {
-          enable = true,
-          -- disable highlight for large files
-          disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+            vim.opt.autoindent = false
+        end,
+    },
+    {
+        -- show the context of the currently visible buffer contents
+        -- show you which function you are looking at
+        "nvim-treesitter/nvim-treesitter-context",
+        event = "BufReadPost",
+        opts = {
+            max_lines = 3,
+            min_window_height = 20,
+            line_numbers = true,
+            multiline_threshold = 1,
+            -- Which context lines to discard if `max_lines` is exceeded.
+            trim_scope = "outer",
+            -- Line used to calculate context. Choices: 'cursor', 'topline'
+            mode = "cursor",
         },
-        indent = {
-          enable = true,
-          disable = {},
+        keys = {
+            {
+                "[c",
+                function()
+                    require("treesitter-context").go_to_context()
+                end,
+                desc = "Jump to The Beginning of the Function",
+            },
         },
-        -- increase selection scope gradually
-        incremental_selection = {
-          enable = false,
-        },
-      }
-    end
-  },
-  {
-    -- show the context of the currently visible buffer contents
-    -- show you which function you are looking at
-    "nvim-treesitter/nvim-treesitter-context",
-    config = function()
-      local tscontext = require('treesitter-context')
-      tscontext.setup {
-        enable = true,
-        -- How many lines the window should span.
-        max_lines = 0,              -- no limit
-        -- Minimum editor window height to enable context.
-        min_window_height = 0,      -- no limit
-        line_numbers = true,
-        -- Maximum number of lines to collapse for a single context line
-        multiline_threshold = 20,   -- more than this will be folded
-        -- Which context lines to discard if `max_lines` is exceeded.
-        -- Choices: 'inner', 'outer'
-        trim_scope = 'outer',
-        -- Line used to calculate context. Choices: 'cursor', 'topline'
-        mode = 'cursor',
-        -- Separator between context and content.
-        -- Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when
-        -- there are at least 2 lines above cursorline.
-        separator = nil,
-        -- The Z-index of the context window
-        zindex = 20,
-        -- (fun(buf: integer): boolean) return false to disable attaching
-        on_attach = nil,
-      }
-      vim.keymap.set("n", "[c", function()
-        tscontext.go_to_context()
-      end, { silent = true })
-    end
-  },
+    },
 }
