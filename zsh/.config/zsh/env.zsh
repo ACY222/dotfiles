0e953cca66f6302f40b1881814d39054d478628d
ACY222 <838113915@qq.com>
2026-03-08 16:27:53 +0800

    move environment variables from zshrc to env.zsh

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -4,3 +4,6 @@ export VISUAL="nvim"
 export XDG_CONFIG_HOME=$HOME/.config
 export PATH="/opt/homebrew/bin:$PATH"
 export STARSHIP_CONFIG=~/.config/starship/starship.toml
+
+export TLDR_AUTO_UPDATE_DISABLED=1
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
