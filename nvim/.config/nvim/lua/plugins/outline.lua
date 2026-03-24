90190efc9a70a1d7c7b58282104ed194ac0d3b08
ACY222 <838113915@qq.com>
2026-03-24 16:21:01 +0800

    ban aerial in some filetypes

--- nvim/.config/nvim/lua/plugins/outline.lua
+++ nvim/.config/nvim/lua/plugins/outline.lua
@@ -7,6 +7,12 @@ return {
     opts = {
         -- use `{` and `}` to jump between functions
         on_attach = function(bufnr)
+            -- banned in some filetypes
+            local ft = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+            if ft == "typst" or ft == "markdown" then
+                return
+            end
+
             vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
             vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
         end,
