75cbc0495c9ac786c3fa1ee75017df22c50f31af
ACY222 <838113915@qq.com>
2026-04-27 19:41:44 +0800

    remove noice.nvim (now I can run shell commands in nvim)

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -24,7 +24,6 @@ require("lazy").setup({
     -- ui
     require("plugins.colorscheme"), -- colorscheme
     require("plugins.indent"), -- show indent line
-    require("plugins.noice"), -- popup notice
     require("plugins.bufferline"), -- buffer line
     require("plugins.statusline"), -- statusline
     require("plugins.windows-separator"), -- more distinguishable separators
@@ -41,13 +40,14 @@ require("lazy").setup({
     require("plugins.surround"), -- add/change/remove surround
     require("plugins.undo"), -- undo tree
     require("plugins.markdown"), -- preview, table mode
-    require("plugins.latex"),
+    require("plugins.latex"), -- latex auto compile
     require("plugins.typst"), -- typst preview
     require("plugins.img-clip"), -- paste images in markup languages
     require("plugins.conform"), -- enable formatter
     require("plugins.trim-space"), -- auto remove space in the end
     require("plugins.trouble"), -- showing diagnostics, references, quickfix and so on
     require("plugins.hardtime"), -- practice your vim skills
+    require("plugins.opencode"), -- enable opencode
     require("plugins.debug"),
     -- navigation
     require("plugins.flash"), -- enhanced f, t, and word-search
