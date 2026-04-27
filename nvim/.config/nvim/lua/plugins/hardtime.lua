9a34104818d4c686c86dbb365d354c99c7e06560
ACY222 <838113915@qq.com>
2026-04-27 19:40:49 +0800

    enable arrow keys

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -4,5 +4,12 @@ return {
     dependencies = { "MunifTanjim/nui.nvim" },
     opts = {
         disable_mouse = false,
+        -- enable arrow keys
+        disabled_keys = {
+            ["<Up>"] = false,
+            ["<Down>"] = false,
+            ["<Left>"] = false,
+            ["<Right>"] = false,
+        },
     },
 }
